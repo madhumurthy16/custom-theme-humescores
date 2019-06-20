b7263a35f845464a0126848298a44d07ef111952
Madhu Murthy <msmurthy@msn.com>
2019-06-20 14:12:34 -0700

    Initial Commit

---
+++ wp-content/themes/humescores/header.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Humescores
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'humescores' ); ?></a>
+	
+	<?php if ( get_header_image() && is_front_page() ) : ?>
+		<figure class="header-image">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+				<?php the_header_image_tag(); ?> 
+			</a>
+		</figure> <!-- .header-image -->
+	<?php endif // End header image check.	?>
+
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			
+			<?php the_custom_logo(); ?>
+			<div class="site-branding__text">
+				<?php
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+				$humescores_description = get_bloginfo( 'description', 'display' );
+				if ( $humescores_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $humescores_description; /* WPCS: xss ok. */ ?></p>
+				<?php endif; ?>
+			</div> <!-- .site-branding__text -->
+		</div><!-- .site-branding -->
+
+
+
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'humescores' ); ?></button>
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+			) );
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
